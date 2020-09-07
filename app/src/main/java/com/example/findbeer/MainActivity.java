bcdd41c6bef407349d6768eebaeb9befc18b7cf3
Danial <danial.200316@gmail.com>
2020-09-07 17:45:15 +0600

    android project

---
+++ app/src/main/java/com/example/findbeer/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.findbeer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.hfad.beeradviser.beerExpert;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private  beerExpert expert=new beerExpert();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickFindBeer(View view){
+        TextView brands=(TextView) findViewById(R.id.brands);
+        Spinner color=(Spinner) findViewById(R.id.color);
+        String beerType=String.valueOf(color.getSelectedItem());
+        List<String> brandsList=expert.getBrands(beerType);
+        StringBuilder brandsFormatted=new StringBuilder();
+        for (String brand:brandsList){
+            brandsFormatted.append(brand).append('\n');
+        }
+        brands.setText(brandsFormatted);
+    }
+
+}
